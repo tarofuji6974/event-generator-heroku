a02ade3bbf79c60c76b2bf57219c4eecbf400d3c
tarofuji6974 <amazarashi2525@icloud.com>
2020-01-06 02:38:36 +0900

    init

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module EventGenerator
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
+
+    #rails gコマンド実行時に、スペックファイルを自動で追加する
+    config.generators do |g|
+        g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+     end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
